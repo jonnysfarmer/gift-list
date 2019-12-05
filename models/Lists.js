f9a0b26493d909244998d9becf3541f2ea10715e
JenniKate Wallace <jennikate@gmail.com>
2019-12-05 12:08:38 +0000

    seed template & list data

--- models/Lists.js
+++ models/Lists.js
@@ -2,8 +2,8 @@
 const mongoose = require('mongoose')
 
 const listSchema = new mongoose.Schema({
-  //userId connects us to the user schema
-  userId: { type: mongoose.Schema.ObjectId, ref: 'User', required: true },
+  //userId connects us to the USER schema
+  user: { type: mongoose.Schema.ObjectId, ref: 'User', required: true },
   listName: { type: String, required: true },
   //this must be 'me' or anyone else you enter
   giftRecipient: { type: String, required: true },
@@ -12,14 +12,14 @@ const listSchema = new mongoose.Schema({
   eventDate: { type: Date },
   eventReminder: { type: Boolean },
   //budget is also optional, but must be turned to the correct format for passing on APIs by the front end
-  budget: { type: Number },
+  budget: { type: String }, //budget is always stored as GBP
   //list status is either active (visible) or archived (not visible) on dashboard/shared list view
   listStatus: { type: String, enum: ['Active', 'Archived'] },
-  //categories (all are subcategories) - subcategoryId connects us to the category schema, we use subcategory here as that's what we'd query from the 
-  subcategoryId: { type: Array, default: [] },
+  //CATEGORY (all are subcategories) - subcategoryId connects us to the category schema, we use subcategory here as that's what we'd query from the 
+  subcategory: { type: Array, default: [] },
   //we're not implementing keywords in MVP but including in data structure for future
   keywords: { type: Array, default: [] },
-  //items saved will be an array of ID's fro our items schema as that is where the item deatils are saved
+  //ITEMS saved will be an array of ID's fro our items schema as that is where the item deatils are saved
   itemsSaved: { type: Array, default: [] },
   //shareable url is a url that will show a readonly view of the giftlist saved items
   shareUrl: { type: String }
