70936f2e8e6721f5b2ee4bbf48523ee76fe4a009
Georg Preuss <preussg@gmail.com>
2019-12-05 12:44:31 +0000

    category schema

---
+++ models/Category.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const categorySchema = new mongoose.Schema({
+  categoryName: { type: String, required: true },
+  etsyCategoryName: { type: String, required: true },
+  subcategory: [subcategorySchema]
+})
+
+module.exports = mongoose.model('Category', categorySchema)
+
+const subcategorySchema = new mongoose.Schema({
+  subcategoryName: { type: String, required: true },
+  etsysubcategoryName: { type: String, required: true },
+  subcategoryID: { type: mongoose.Schema.ObjectId, ref: 'List', required: true }
+})
+
+module.exports = mongoose.model('Subcategory', subcategorySchema)
