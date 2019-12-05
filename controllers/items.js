cda2415b3ce190b07b019a9cdfc0391757957024
JenniKate Wallace <jennikate@gmail.com>
2019-12-05 19:16:35 +0000

    calling show from add

--- controllers/items.js
+++ controllers/items.js
@@ -1,21 +1,19 @@
 const Item = require('../models/Item')
 const axios = require('axios')
-
-
-//Functions required just by this backend controller
-
-
-
+axios.defaults.baseURL = 'http://localhost:8000/api'
 
 
 
 function add(req, res) {
 
-  //receive API request and get the variables we need
-  console.log(req.body)
-  const listingId = `${req.body.store}-${req.body.id}`.toLowerCase()
-  console.log(listingId)
-
+  //call the show function via the router so we can pass the listingId on the url (there must be a better way than this!)
+  axios.get(`/items/${req.body.store}-${req.body.id}`)
+    .then(console.log('calling it fine'))
+    //if no error, then send res to say successfully saved (need to change this to call the post listingId to list
+    .then(res => console.log('exists in db'))
+    //if is error then we'll run our post
+    //axios is going to error here if the url is bad, aka the item doesn't exist in the database
+    .catch(err => console.log('I found bad'))
 }
 
 
@@ -23,9 +21,7 @@ function add(req, res) {
 
 
 
-
-
-//get all items from our db
+//===== GET ALL ITEMS =====
 function index(req, res) {
   Item
     .find()
@@ -33,17 +29,18 @@ function index(req, res) {
     .catch(err => console.log(err))
 }
 
-//get specific item from our db
+//===== GET SPECIFIC ITEM =====
 function show(req, res) {
+  // console.log(listingId)
   //turn listing id into an obj so we can use find
-  const json = '{"listingId" : "' + req.params.listingId + '"}' 
+  const listingId = req.params.listingId
+  const json = '{"listingId" : "' + listingId + '"}'
   const itemListingId = JSON.parse(json)
-  console.log(req.params.listingId)
 
   //get item by listingId
   Item
     .findOne(itemListingId)
-  //return error if missing or success if success
+    //return error if missing or success if success
     .then(item => {
       //check record exists and respond with error if it doesn't
       if (!item) res.status(404).json({ status: 404, message: 'Not found', error: 'Not found' })
