20b3f753f87babc9da6b26e385f985b4d2007eb9
Georg Preuss <preussg@gmail.com>
2019-12-06 10:45:17 +0000

    added etsy call to get categories and subcategories

--- app.js
+++ app.js
@@ -4,7 +4,14 @@ const bodyParser = require('body-parser')
 const { port, dbURI } = require('./config/environment')
 const router = require('./router')
 const cors = require('cors')
+const axios = require('axios')
+const categories = require('./controllers/categories')
 
+categories.then((res)=>{
+  const categories = res
+  console.log(categories)
+  // console.log('Categories :', res)
+})
 
 mongoose.connect(dbURI, {
   useNewUrlParser: true,
@@ -25,5 +32,22 @@ app.use(cors())
 //a different port
 
 // app.use('/api', router)
+
+
+
+// export this to categories later and hide api key
+// axios.get('https://openapi.etsy.com/v2/taxonomy/categories/?api_key=0b6tytx6ibc1jzi7gd790l0a')
+//   .then(res => {
+//     const categories = res.data.results.map(elem => elem.name)
+//     console.log(categories)
+//     categories.forEach((elem,i) => {
+//       setTimeout(() => {
+//         axios.get(`https://openapi.etsy.com/v2/taxonomy/categories/${elem}?api_key=0b6tytx6ibc1jzi7gd790l0a`)
+//           .then(res => console.log(res.data.results.map(elem => elem.name)))
+//           .catch(err => console.log(err))
+//           // 1s seems to be still be exceeding the api call quota, so leave at 2s for now
+//       }, 2000 * i)
+//     })
+//   })
 
 app.listen(port, () => console.log(`Green light on port ${port}`))
