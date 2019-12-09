aa87dd292eb1d8a0337519d69d7d25073db1c774
Georg Preuss <preussg@gmail.com>
2019-12-09 13:55:29 +0000

    added nav and home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import 'bulma/css/bulma.css'
 import './styles/main.scss'
 
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import Browse from './components/Browse'
+
 function App() {
   return (
-    <div className="App">
-        Hello World
-    </div>
+    <BrowserRouter>
+    <Navbar />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/browse" component={Browse} />
+    </Switch>
+    </BrowserRouter>
   )
 }
 
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,82 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+// import Auth from
+
+
+const Navbar = () => {
+
+  const [state, setState] = useState({
+    isOpen: false
+  })
+
+  // const handleLogout = () => {
+  //   Auth.logout()
+  //   props.history.push('/')
+  // }
+
+  const toggleNavbar = () => {
+    setState({ isOpen: !state.isOpen })
+  }
+
+  return <div className="navbar">
+    <div className="container">
+      <div className="navbar-brand">
+        <Link className="navbar-item" to="/"
+          onClick={() => setState({ isOpen: false })}
+        >
+          Gift List
+          </Link>
+        <a
+          role="button"
+          className={`navbar-burger burger ${state.isOpen ? 'is-active' : ''}`}
+          onClick={() => toggleNavbar()}
+          aria-label="menu"
+          aria-expanded="false"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+      <div className={`navbar-menu ${state.isOpen ? 'is-active' : ''}`}>
+        <div className="navbar-end">
+          <div className="navbar-item">
+            <Link className="navbar-item" to="/browse"
+              onClick={() => toggleNavbar()}
+            >
+              Browse
+              </Link>
+          </div>
+          <div className="navbar-item">
+            <Link className="navbar-item" to="/create-list"
+              onClick={() => toggleNavbar()}
+            >
+              Create new list
+              </Link>
+          </div>
+          <div className="navbar-item">
+            <Link className="navbar-item" to="/register"
+              onClick={() => toggleNavbar()}
+            >
+              Register
+              </Link>
+          </div>
+          <div className="navbar-item">
+            <Link className="navbar-item" to="/login"
+              onClick={() => toggleNavbar()}
+            >
+              Login
+            </Link>
+          </div>
+          {/* {Auth.isAuthorized() && <div className="navbar-item">
+              <div className="navbar-item" onClick={() => handleLogout()}>
+                Logout
+              </div>
+            </div>} */}
+        </div>
+      </div>
+    </div>
+  </div>
+}
+
+export default Navbar
