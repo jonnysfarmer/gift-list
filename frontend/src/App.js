e5a23e14c222aaa6d6d1a7238303ba478523b67a
jonnysfarmer <john.farmer@latimerross.com>
2019-12-09 11:55:37 +0000

    updated backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,55 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import Auth from './lib/auth'
+// import Auth from './lib/auth'
 
 import 'bulma/css/bulma.css'
 import './styles/main.scss'
 
+
+
 function App() {
 
-  //Need to find out what user is viewing - via local token
-  //get the user ID from the local token
-  //API call for user Info
-  // List data from Local user.
+  const [userInfo, setUserInfo] = useState({})
+  const [listInfo, setListInfo] = useState([])
+  const [errors, setErrors] = useState([])
+  
+  //temp for devlopment, this needs to be passed via URL
+  const userID = '5dee2efa789b9a57f9799cff'
 
+  //this hook gets the user Info
+  const userlistHook = () => {
+    //we need userID = props.params.match.id
+    axios.get(`http://localhost:8000/api/lists/${userID}`)
+    .then(response => {
+      setListInfo(response.data)
+    })
+    .catch(err => setErrors(err))
+  }
+  const userInfoHook = () => {
+    //we need userID = props.params.match.id
+    axios.get(`http://localhost:8000/api/user/${userID}`)
+    .then(response => {
+      setUserInfo(response.data)
+    })
+    .catch(err => setErrors(err))
+  }
+
+
+
+
+
+  useEffect(userlistHook, [])
+  useEffect(userInfoHook, [])
+  // console.log(listInfo[0].user.firstname)
+
+  console.log(listInfo)
+  console.log(userInfo)
+  if(listInfo === [] || userInfo === {} ) return <div>Loading</div>
   return (
     <section className="section">
       <section className="section">
         <div className="container">
-        Basic Info
+        <div className='title'>Welcome back, {userInfo.firstname}</div>
         </div>
       </section>
       <section className="section">
