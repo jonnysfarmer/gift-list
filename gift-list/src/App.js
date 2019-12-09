a6c4310e17ed0e994c098ba00d771733963b488e
jonnysfarmer <john.farmer@latimerross.com>
2019-12-09 10:04:15 +0000

    frontend setup

---
+++ gift-list/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import 'bulma/css/bulma.css'
+import './styles/main.scss'
+
+function App() {
+  return (
+    <div className="App">
+        Hello World
+    </div>
+  )
+}
+
+export default App;
