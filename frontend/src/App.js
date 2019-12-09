8ff5d5313837be04f6664a25401f85f287781b77
JenniKate Wallace <jennikate@gmail.com>
2019-12-09 10:57:06 +0000

    createform layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,9 +4,70 @@ import 'bulma/css/bulma.css'
 import './styles/main.scss'
 
 function App() {
+  //the aside of suggested lists is hardcoded here for now as it was an additional feature we thought of that was simple to implement in this manner
+  //we want to extract this out to the backend so that here we just make an API call to get the latest 9 suggestions
+  
   return (
     <div className="App">
-        Hello World
+
+      <section className='section'>
+        <div className='container'>
+          <h1>Create a list</h1>
+            <div className='container columns'>
+
+              <form className='form column'>
+
+              </form>
+
+              <aside className='aside column columns'>
+                <h2>Start with one of our lists</h2>
+
+                <div className='column'>
+                  <img alt='imageName'/>
+                  <span>img above, text over img</span>
+                </div>
+                <div className='column'>
+                  <img alt='imageName'/>
+                  <span>img above, text over img</span>
+                </div>
+                <div className='column'>
+                  <img alt='imageName'/>
+                  <span>img above, text over img</span>
+                </div>
+                <div className='column'>
+                  <img alt='imageName'/>
+                  <span>img above, text over img</span>
+                </div>
+                <div className='column'>
+                  <img alt='imageName'/>
+                  <span>img above, text over img</span>
+                </div>
+                <div className='column'>
+                  <img alt='imageName'/>
+                  <span>img above, text over img</span>
+                </div>
+                <div className='column'>
+                  <img alt='imageName'/>
+                  <span>img above, text over img</span>
+                </div>
+                <div className='column'>
+                  <img alt='imageName'/>
+                  <span>img above, text over img</span>
+                </div>
+                <div className='column'>
+                  <img alt='imageName'/>
+                  <span>img above, text over img</span>
+                </div>
+
+              </aside>
+
+            </div>
+        </div>
+      </section>
+
+
+
+
     </div>
   )
 }
