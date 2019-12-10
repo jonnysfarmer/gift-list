354d341c2baf83cea6e92046dc8c535f9aca0072
jonnysfarmer <john.farmer@latimerross.com>
2019-12-10 09:02:01 +0000

    key working

--- backend/controllers/etsyProducts.js
+++ backend/controllers/etsyProducts.js
@@ -1,11 +1,11 @@
 
 const axios = require('axios')
 require('dotenv').config()
-// const etsyKey = process.env.ETSY_KEY
+const etsyKey = process.env.ETSY_KEY
 
 function getProducts(req, res) {
   const cat = req.params.catname
-  axios.get(`http://openapi.etsy.com/v2/listings/active/?region=GB&category=${cat}&limit=6&api_key=0b6tytx6ibc1jzi7gd790l0a`)
+  axios.get(`http://openapi.etsy.com/v2/listings/active/?region=GB&category=${cat}&limit=6&api_key=${etsyKey}`)
     .then(response => {
       // console.log(response.data)
       res.send({ data: response.data.results })
@@ -17,7 +17,7 @@ function getProducts(req, res) {
 function getSubCat(req, res) {
   const cat = req.params.catname
   const subcat = req.params.subcatname
-  axios.get(`http://openapi.etsy.com/v2/listings/active/?region=GB&category=${cat}/${subcat}&limit=6&api_key=0b6tytx6ibc1jzi7gd790l0a`)
+  axios.get(`http://openapi.etsy.com/v2/listings/active/?region=GB&category=${cat}/${subcat}&limit=6&api_key=${etsyKey}`)
     .then(response => {
       // console.log(response.data)
       res.send({ data: response.data.results })
@@ -29,7 +29,7 @@ function getSubCat(req, res) {
 
 function getImage(req, res) {
   const id = req.params.id
-  axios.get(`http://openapi.etsy.com/v2/listings/${id}/images?region=GB&api_key=0b6tytx6ibc1jzi7gd790l0a`)
+  axios.get(`http://openapi.etsy.com/v2/listings/${id}/images?region=GB&api_key=${etsyKey}`)
     .then(response => {
       res.send({ image: response.data.results[0].url_570xN }) 
     })
