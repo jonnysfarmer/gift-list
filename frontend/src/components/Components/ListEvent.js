8e16d9ffa93f7aa87497ff7adca51811aad4fe75
JenniKate Wallace <jennikate@gmail.com>
2019-12-11 15:47:05 +0000

    hiding empty fields

--- frontend/src/components/Components/ListEvent.js
+++ frontend/src/components/Components/ListEvent.js
@@ -12,6 +12,7 @@ const ListEvent = (props) => {
   //===== ICONOGRAPHY =====
   const editIcon = <FontAwesomeIcon icon={faEdit} />
 
+
   //===== VARIABLES =====
   const [data, setData] = useState({})
   const [tempData, setTempData] = useState() //on handle change we set the temp date into here so we can either save it, or cancel changes
@@ -62,38 +63,37 @@ const ListEvent = (props) => {
   return (
 
     <div id='list-event'>
-
       <div className={`element ${editState ? 'hide' : 'show'}`}>
-        <h2><span onClick={() => { canEdit(true) }}>{editIcon}</span> {data.eventName}</h2>
-        <p>on {moment(data.eventDate).format('DD-MM-YYYY')}</p>
-        {data.eventReminder && <p>We will send you a reminder</p>}
+        {data.eventName && <h2><span onClick={() => { canEdit(true) }}>{editIcon}</span> {data.eventName}</h2>}
+        {data.eventDate && <p>on {moment(data.eventDate).format('DD-MM-YYYY')}</p>}
+        {!data.eventReminder && <p>We will send you a reminder</p>}
       </div>
 
       <form className={`form ${editState ? 'show' : 'hide'}`}>
-        <div className="field">
+        {data.eventName && <div className="field">
           <label htmlFor="eventName" className="label">
             Your event name
           </label>
           <div className="control">
             <input className='input' type='text' name='eventName' defaultValue={data.eventName} onChange={((e) => handleChange(e))} />
           </div>
-        </div>
-        <div className="field">
+        </div>}
+        {data.eventDate && <div className="field">
           <label htmlFor="eventDate" className="label">
             Your event date
           </label>
           <div className="control">
             <input className='input' type='date' defaultValue={moment(data.eventDate).format('YYYY-MM-DD')} name='eventDate' onChange={((e) => handleChange(e))} />
           </div>
-        </div>
-        <div className="field">
+        </div>}
+        {data.eventReminder && <div className="field">
           <label htmlFor="eventReminder" className="label">
             Your event reminders are
           </label>
           <div className="control">
             <input className='checkbox' type='checkbox' defaultValue={data.eventReminder} name='eventReminder' onChange={((e) => handleChange(e))} />
           </div>
-        </div>
+        </div>}
         <button className='button save' onClick={((e) => handleSave(e))} >Save changes</button>
         <button className='button cancel' onClick={((e) => handleCancel(e))}>Cancel changes</button>
       </form>
