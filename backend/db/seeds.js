fde50a4e8064eb9e2e039396b039c4778757172c
Georg Preuss <preussg@gmail.com>
2019-12-12 10:34:39 +0000

    backend fixes and dropdown menu

--- backend/db/seeds.js
+++ backend/db/seeds.js
@@ -108,7 +108,7 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
         return getEtsyCategories()
       })
       .then(categoryResp => {
-        // console.log(categoryResp)
+        console.log('123 ', categoryResp[0])
         return Category.create(
           categoryResp
         )
